1c6bc280585a0bf83c4a7593dc80194a225c6522
marcol13 <marcinkrueger@gmail.com>
2024-04-07 01:26:28 +0200

    add relations example in notebook

--- milp_electre/core/relations.py
+++ milp_electre/core/relations.py
@@ -1,5 +1,6 @@
 from mcda.core.relations import Relation, PreferenceRelation, IncomparableRelation, IndifferenceRelation
 
+# TODO: Keep information about relations order
 class PositivePreference(PreferenceRelation):
     short_name = "P+"
     full_name = "Positive Preference"
