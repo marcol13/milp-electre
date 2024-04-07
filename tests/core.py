75d023d31edef4b10afd423be4b18654491c04fd
Marcin <marcinkrueger@onet.eu>
2024-04-08 01:24:51 +0200

    add simple relations test using pytest

---
+++ tests/core.py
@@ -0,0 +1,19 @@
+from milp_electre.core.relations import PositivePreference, NegativePreference, Indifference, Incomparible
+
+class TestRelations:
+    def test_positive_preference(self):
+        relation = PositivePreference(1, 0)
+        assert relation.compatible(PositivePreference(1, 0)) == True
+        assert relation.compatible(NegativePreference(1, 0)) == False
+        assert relation.compatible(Indifference(1, 0)) == False
+        assert relation.compatible(Incomparible(1, 0)) == False
+
+        assert relation.compatible(PositivePreference(0, 1)) == False
+        assert relation.compatible(NegativePreference(0, 1)) == True
+        assert relation.compatible(Indifference(0, 1)) == False
+        assert relation.compatible(Incomparible(0, 1)) == False
+
+        assert relation.compatible(PositivePreference(1, 2)) == True
+        assert relation.compatible(NegativePreference(1, 2)) == True
+        assert relation.compatible(Indifference(1, 2)) == True
+        assert relation.compatible(Incomparible(1, 2)) == True
