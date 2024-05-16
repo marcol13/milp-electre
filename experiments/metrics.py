b4090f626398b8619d31ec6bd13df67490f8ca52
marcol13 <marcinkrueger@gmail.com>
2024-05-17 00:01:14 +0200

    add max rank diff

--- experiments/metrics.py
+++ experiments/metrics.py
@@ -1,18 +1,26 @@
 import numpy as np
 
-def kendall_tau(x, y):
+def kendall_distance(x, y):
     shape = x.shape
     assert shape == y.shape
     if shape == None:
         return 1.0
     x = np.asarray(x)
     y = np.asarray(y)
-    dis = 0
+    distance = 0
     for i in range(shape[0]):
         for j in range(shape[1]):
-            dis += abs(x[i, j] - y[i, j])
+            distance += abs(x[i, j] - y[i, j])
     
-    dis *= 1/2
-    kendall_coef = 1 - 4 * dis / (shape[0] * (shape[0] - 1))
+    distance *= 1/2
+    return distance
+
+def kendall_tau(distance, n):
+    kendall_coef = 1 - 4 * distance / (n * (n - 1))
     return kendall_coef
 
+def max_rank_diff(size: int):
+    if size % 2 == 0:
+        return (size // 2) * size
+    else:
+        return -(size // -2) * (size - 1)
